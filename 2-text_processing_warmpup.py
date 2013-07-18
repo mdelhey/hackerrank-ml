85364b5799f2d089f7235ba826169e862e03c641
Matt Delhey <mdelhey@tripadvisor.com>
2013-07-18 17:21:43 -0400

    add 2

--- 2-text_processing_warmpup.py
+++ 2-text_processing_warmpup.py
@@ -1,53 +1,59 @@
 import sys
 import re
 
+stdin = sys.stdin.readlines()
+
 def count_a(text):
 	pattern = "[ ]{1,2}[aA]{1}[ ]{1,2}"
-	# Grab results, count them, return.
 	results = re.findall(pattern, text)
 	count = len(results)
 	return(count)
 
 def count_an(text):
 	pattern = "[ ]{1,2}[aA]{1}[n]{1}[ ]{1,2}"
-	# Grab results, count them, return.
 	results = re.findall(pattern, text)
 	count = len(results)
 	return(count)
 
 def count_the(text):
-	pattern = "[ ]{1,2}[tT]{1}[h]{1}[e]{1}[ ]{1,2}"
-	# Grab results, count them, return.
+	pattern = "([ ,?.!\'\"]{1,2}[t]{1}|[T]{1})[h]{1}[e]{1}[ ]{1,2}"
 	results = re.findall(pattern, text)
 	count = len(results)
 	return(count)
 
 def count_dates(text):
-	# mdy or dmy
 	def count_allnumeric():
-		pattern = "[ ]{1,2}[1-9]{1,2}[./- _]{0,1}[1-9]{1,2}[./- _]{0,1}[1-9]{2,4}"
+		# month day year
+		pattern = "[0-9]{1,2}[ \.\-_/]{1}[0-9]{1,2}[ \.\-_/]{1}[0-9]{2,4}"
 		results = re.findall(pattern, text)
 		count = len(results)
 		return(count)
 	
-	#def count_allwords():
-		#pattern = "[1-9]+[th|rd|st}[ ]{1,2}[a-zA-z]+"
-		#results = re.findall(pattern, text)
-		#count = len(results)
-		#return(count)
+	def count_allwords():
+		# day month year
+		pattern = "[0-9]{1,2}[thrdns]{0,2}[ ]{1,2}[a-zA-Z]{3,9}[ \-,]{1,2}[0-9]{2,4}"
+		results = re.findall(pattern, text)
+		count = len(results)
+		return(count)
+
+	def count_allwords2():
+		# month day year
+		pattern = "[a-zA-Z]{3,9}[ ,]{1,2}[0-9]{1,2}[ ,]{1,2}[0-9]{2,4}"
+		results = re.findall(pattern, text)
+		count = len(results)
+		return(count)
 	
-	#total_count = count_allnumeric() + count_allwords()
-	total_count = count_allnumeric()
+	total_count = count_allnumeric() + count_allwords() + count_allwords2()
 	return(total_count)
 
-#text = "New York is a state in the Northeastern region of the United States. New York is the 27th-most extensive, the 3rd-most populous, and the 7th-most densely populated of the 50 United States."
-stdin = sys.stdin.readlines()
 
 # Get arguments using stdin
 N = int(stdin[0])
 for i in range(2*N)[1::]:
+	# Cycle through each actual text
 	if (i % 2) != 0:
 		text = stdin[i]
+		# Print results for each function
 		print(count_a(text))
 		print(count_an(text))
 		print(count_the(text))
